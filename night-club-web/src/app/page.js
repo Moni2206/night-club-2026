505df7cef1173b52331d769d5a38188dfaf080d0
Monika <moha0009@stud.kea.dk>
2026-05-04 17:42:14 +0200

    The main menu is more or less finished, but the responsive mobile version isn’t quite optimal.”

---
+++ night-club-web/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-white dark:bg-black">Home</h1>
+      <img src="/assets/bg/header_bg_2.jpg" alt="header" className="h-auto w-auto" />{" "}
+    </div>
+  );
+}
